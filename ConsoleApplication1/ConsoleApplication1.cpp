dfce191412acf33266d3582fc1c998e49d0279bf
blkph0x <blkph0x@BLKPH0X-PC>
2018-03-03 20:18:05 +1100

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int hidden() {
+	system("cmd");
+	return 0;
+}
+void func(char *A) {
+	
+	printf(A);
+	return;
+}
+
+int main()
+{
+    
+	char overflow[25];
+	char junk[1024] = { overflow[25] };
+ cin >> overflow;
+	if(0) {
+		hidden();
+	}else
+	func(overflow);
+	
+	return 0;
+	
+}
+
+
+
